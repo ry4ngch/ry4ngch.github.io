e4227d6362e901f41615b71894de6ba9deee2a86
Ryan Goh <ry4n.gch@gmail.com>
2026-01-10 17:09:46 +0800

    fixed: timeline line css

--- assets/js/timeline.js
+++ assets/js/timeline.js
@@ -1,32 +1,3 @@
-const loadTimelineLine = () => {
-  const timelines = document.querySelectorAll('.resume_wrapper .timeline');
-    
-  if (timelines) {
-    timelines.forEach(function(ob) {
-      let divHeight = 0;
-      const current_timeline_line = ob.querySelector('.line');
-      const timeline_childEl = ob.querySelectorAll('.timeline-item:not(:last-child)');
-      
-      if (timeline_childEl.length > 0) {
-        timeline_childEl.forEach(function(item) {
-          const itemRect = item.getBoundingClientRect();
-          const itemComputedStyle = window.getComputedStyle(item);
-          
-          // Get the height of the element including its padding and border
-          let itemHeight = itemRect.height;
-
-          // Add the bottom margin to the height
-          itemHeight += parseFloat(itemComputedStyle.marginBottom);
-          
-          divHeight += itemHeight + 7;
-        });
-
-        current_timeline_line.style.height = divHeight + 'px';
-      }
-    });
-  }
-}
-
 const toggleDetail = (event, contentId) => {
   const parentEl = document.getElementById(contentId).parentElement;
   console.log(event.target);
@@ -34,11 +5,3 @@ const toggleDetail = (event, contentId) => {
   const detailInfoEl = parentEl.querySelector('.section-info');
   detailInfoEl.classList.toggle('expand-section');
 }
-
-window.addEventListener('DOMContentLoaded', function() {
-  loadTimelineLine();
-});
-
-window.addEventListener('resize', (event) => {
-  loadTimelineLine();
-})
